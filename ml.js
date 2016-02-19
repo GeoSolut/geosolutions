46cf0969a9379df33ef653c3c2876cace912615f
GeoSolut <geosolutions@mail.com>
2016-02-19 15:19:48 +0100

    Added files via upload

---
+++ ml.js
@@ -0,0 +1,7 @@
+function em_click(){
+location.href="mailto:info@geosol.net";
+return true; }
+function ml_write(){
+document.write('info@geosol.net');}
+function ml_hp_write(){
+document.write('<img title="info@geosol.net"');}
